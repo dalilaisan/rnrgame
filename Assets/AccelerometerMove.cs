ab2538f6ee29e042b0394b6796ec9ebcaf85c90a
dalilaisan <dalila.isan@gmail.com>
2018-05-04 22:28:57 +0200

    character sliding and water asset addition

---
+++ Assets/AccelerometerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerMove : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float temp = Input.acceleration.x;
+        //Debug.Log(temp);
+       // transform.Translate(temp * speed * Time.deltaTime, 0, 0);
+       // transform.Translate(Input.acceleration.x * Time.smoothDeltaTime * 10, 0, 0);
+
+        transform.Translate(new Vector2(Input.acceleration.x, 0) * Time.deltaTime * 10f);
+    }
+}
